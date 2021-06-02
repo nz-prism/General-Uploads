35cfb3b6ddd4aa2d8bb21df42ea74551188bc333
nz_prism <83767318+nz-prism@users.noreply.github.com>
2021-06-02 18:02:27 +0900

    Add files via upload

---
+++ PluginTemplate.js
@@ -0,0 +1,37 @@
+//=============================================================================
+// RPG Maker MZ - 
+//=============================================================================
+
+/*:
+ * @target MZ
+ * @plugindesc 
+ * @author 
+ * @url
+ *
+ * @help 
+ *
+ */
+
+/*:ja
+ * @target MZ
+ * @plugindesc 
+ * @author 
+ * @url
+ *
+ * @help 
+ *
+ */
+
+(() => {
+    'use strict';
+
+
+    Window_PartyCommand.prototype.itemTextAlign = function() {
+        return "right";
+    };
+
+    Window_ActorCommand.prototype.itemTextAlign = function() {
+        return "right";
+    };
+
+})();
